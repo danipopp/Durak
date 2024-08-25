577c6e171ae30773b0123d462df8bef895ebebb9
danipopp <popp.daniel16@icloud.com>
2024-08-25 19:17:00 +0200

    Init game

---
+++ game.py
@@ -0,0 +1,24 @@
+# import libraries
+import random
+
+# Constant Cards
+VALUES = ['2','3','4','5','6','7','8','9','10','B','D','K','A']
+SUITS = ['Clubs','Hearts','Spades','Diamonds']
+
+class Cards:
+    # TODO
+    pass
+
+class DeckOfCards:
+    # TODO
+    pass
+
+class Player:
+    # TODO 
+    pass
+
+class Game:
+    # TODO
+    pass
+
+
---
+++ gui.py
@@ -0,0 +1,8 @@
+# import libraries
+
+class DurakGui:
+    # TODO
+    pass
+
+if __name__ == '__main__':
+    pass
