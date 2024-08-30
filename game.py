ec0abf4925e4eb581c497ff6d44b636547f98f6c
danipopp <popp.daniel16@icloud.com>
2024-08-30 15:58:42 +0200

    class Cards and DeckOfCards

--- game.py
+++ game.py
@@ -6,15 +6,23 @@ VALUES = ['2','3','4','5','6','7','8','9','10','B','D','K','A']
 SUITS = ['Clubs','Hearts','Spades','Diamonds']
 
 class Cards:
-    # TODO
-    pass
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+    
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
 
 class DeckOfCards:
-    # TODO
-    pass
+    def __init__(self):
+        self.cards = [Cards(suit,value) for suit in SUITS for value in VALUES]
+        random.shuffle(self.cards)
+
+    def pop_cards(self):
+        return self.cards.pop() if self.cards else None
 
 class Player:
-    # TODO 
+     
     pass
 
 class Game:
