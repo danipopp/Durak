dfcf660cd25b8c1d77c10f207c30fd8d26e9f96d
danipopp <popp.daniel16@icloud.com>
2024-08-31 20:16:12 +0200

    playing is working so far

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ class Cards:
 
 class DeckOfCards:
     def __init__(self):
-        self.cards = [Cards(suit,value) for suit in SUITS for value in VALUES]
+        self.cards = [Cards(value,suit) for suit in SUITS for value in VALUES]
         random.shuffle(self.cards)
 
     def pop_cards(self):
@@ -29,18 +29,23 @@ class Player:
     def draw(self,deck):
         self.hand.append(deck.pop_cards())
 
+    def playCard(self, card):
+        defense = self.hand.pop(card)
+        return defense 
+
     def __repr__(self):
         return f"{self.name}: {', '.join(map(str, self.hand))}"
     
 
 class Game:
-    def __init__(self):
+    def __init__(self, ai_vs_ai = False):
         self.deck = DeckOfCards()
         self.players = [Player("Ksyusha"), Player("Daniel")]
         self.trump = self.deck.cards[-1].suit
         self.dealCards()
         self.currentAttackerIndex = 0
         self.currentDefenderIndex = 1
+        self.ai_vs_ai = ai_vs_ai
         
 
     def dealCards(self):
@@ -48,28 +53,74 @@ class Game:
             for player in self.players:
                 player.draw(self.deck)
 
-    def playGame(self):
+    def playLogic(self):
         attacker = self.players[self.currentAttackerIndex]
         defender = self.players[self.currentDefenderIndex]
 
-        attackCard = attacker.pop(0)
-        print(f"{attacker.name} attacks with {attackCard}")
+        action = -1
+        if self.ai_vs_ai:
+            pass
+        else:
+            action = int(input(f"{attacker.name} choose a card to play (0-{len(attacker.hand)-1}) or -1 for playing no Card: "))
+
+        if action >= 0:
+            attackCard = attacker.playCard(action)
+            print(f"{attacker.name} attacks with {attackCard}")
 
         defenderCard = None
-        for card in defender.hand:
+
+        if self.ai_vs_ai:
+            pass
+        else:
+            print(f"{defender.name} {defender.hand}")
+            defenderCardIndex = int(input(f"{defender.name} choose a card to defend (0-{len(defender.hand)-1}) or -1 for getting all the cards: "))
+        
+        if defenderCardIndex >= 0:
+            card = defender.hand[defenderCardIndex]
             if attackCard.suit != self.trump:
                 if VALUES.index(card.value) > VALUES.index(attackCard.value):
                     defenderCard = card
-                    break
                 elif card.suit == self.trump:
                     defenderCard = card
-                    break
             else:
                 if VALUES.index(card.value) > VALUES.index(attackCard.value):
                     defenderCard = card
-                    break
 
         if defenderCard:
-            # TODO
-            pass
+            defender.playCard(defenderCardIndex)
+            print(f"{defender.name} defends with {defenderCard}")
+        else:
+            defender.hand.append(attackCard)
+            print(f"{defender.name} can't defend. {defender.name} picks up the card(s). ")
+
+        # switch roles
+        self.currentAttackerIndex, self.currentDefenderIndex = self.currentDefenderIndex, self.currentAttackerIndex
+
+        self.drawCards()
+
+        # Check for game over
+        if len(self.deck.cards) == 0 and any(len(player.hand) == 0 for player in self.players):
+            print("Game over!")
+            return True  # Indicate game over
+        
+        self.printCards()
+        return False
+    
+    def drawCards(self):
+        for player in self.players:
+            while len(player.hand) < 6 and self.deck.cards:
+                player.draw(self.deck)
+
+    def printCards(self):
+        for player in self.players:
+            print(player)
+
+    def playGame(self):
+        gameOver = False
+        while not gameOver:
+            gameOver = self.playLogic()
+
+game = Game()
+
+game.playGame()
 
