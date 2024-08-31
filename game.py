8ad95bf6c15c1b8f6c238671feb7e5b6880eeede
danipopp <popp.daniel16@icloud.com>
2024-08-31 15:21:09 +0200

    start playGame

--- game.py
+++ game.py
@@ -34,7 +34,42 @@ class Player:
     
 
 class Game:
-    # TODO
-    pass
+    def __init__(self):
+        self.deck = DeckOfCards()
+        self.players = [Player("Ksyusha"), Player("Daniel")]
+        self.trump = self.deck.cards[-1].suit
+        self.dealCards()
+        self.currentAttackerIndex = 0
+        self.currentDefenderIndex = 1
+        
 
+    def dealCards(self):
+        for i in range(6):
+            for player in self.players:
+                player.draw(self.deck)
+
+    def playGame(self):
+        attacker = self.players[self.currentAttackerIndex]
+        defender = self.players[self.currentDefenderIndex]
+
+        attackCard = attacker.pop(0)
+        print(f"{attacker.name} attacks with {attackCard}")
+
+        defenderCard = None
+        for card in defender.hand:
+            if attackCard.suit != self.trump:
+                if VALUES.index(card.value) > VALUES.index(attackCard.value):
+                    defenderCard = card
+                    break
+                elif card.suit == self.trump:
+                    defenderCard = card
+                    break
+            else:
+                if VALUES.index(card.value) > VALUES.index(attackCard.value):
+                    defenderCard = card
+                    break
+
+        if defenderCard:
+            # TODO
+            pass
 
